5707b6e8973c0e90389db3202bda80eb36cf2339
Your Name <_GoLdEn_BlAcK_X>
2026-05-29 18:23:33 +0500

    feat: complete task manager application with secure jwt flow and env config

--- tasks/viewsets.py
+++ tasks/viewsets.py
@@ -16,3 +16,5 @@ class TaskViewSet(viewsets.ModelViewSet):
         return qs
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    ordering = ['-created_at']
