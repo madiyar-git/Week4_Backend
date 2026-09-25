ec29d05af9a22b93ecb111ffa2464c0ae0de0fad
Madiyar Zh. <_GoLdEn_BlAcK_X>
2026-09-25 18:09:46 +0500

    fix: fix cors problem

--- config/settings.py
+++ config/settings.py
@@ -64,8 +64,8 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",  # for CORS
-    "django.middleware.common.CommonMiddleware",  # for CORS
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
